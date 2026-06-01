28bc5ee359f6a0394b8849cb841ff06f9d464e61
willoughbytucker12 <elioperlman1012@gmail.com>
2026-06-01 16:39:44 +0000

    threat

--- heuristic.hpp
+++ heuristic.hpp
@@ -25,7 +25,7 @@ static double score(const Solution& sol, int wid, int tid, double t,
     (void)t;
     (void)wid;
     (void)exclusive_cnt;
-    return sol.survival(tid);
+    return sol.threat_score.at(tid);
 }
 
 // ---------------------------------------------------------------------------
