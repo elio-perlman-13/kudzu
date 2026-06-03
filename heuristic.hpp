3750b4b7f04b9bd4ca302f01af0490eef3da73f0
willoughbytucker12 <elioperlman1012@gmail.com>
2026-06-03 02:36:45 +0000

    firetime first

--- heuristic.hpp
+++ heuristic.hpp
@@ -141,11 +141,10 @@ Solution& grasp_construction(Solution& sol, double alpha, std::mt19937& rng) {
             if (pit != sol.p_ij->end()) p_this = pit->second;
 
             if (!have_choice
-                || p_this > best_p + eps
-                || (scarcity < best_scarcity && std::fabs(p_this - best_p) <= eps)
-                || (scarcity == best_scarcity && std::fabs(p_this - best_p) <= eps
-                    && (cs.t < chosen_t - eps
-                        || (std::fabs(cs.t - chosen_t) <= eps && wid < chosen_wid)))) {
+                || cs.t < chosen_t - eps
+                || (std::fabs(cs.t - chosen_t) <= eps && scarcity < best_scarcity)
+                || (std::fabs(cs.t - chosen_t) <= eps && scarcity == best_scarcity && p_this > best_p + eps)
+                || (std::fabs(cs.t - chosen_t) <= eps && scarcity == best_scarcity && std::fabs(p_this - best_p) <= eps && wid < chosen_wid)) {
                 have_choice   = true;
                 chosen_wid    = wid;
                 chosen_t      = cs.t;
